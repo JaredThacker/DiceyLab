506d0ee3b009d72e9ad61571f17dc271313823cd
Jared <jared@Zipcoders-MacBook-Pro-3.local>
2024-07-06 16:24:34 -0400

    just simulation left

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,22 @@
+import java.util.HashMap;
 
 public class Bins {
 
+    static HashMap<Integer, Integer> binMap;
+
+    public Bins(Integer low, Integer high){
+        this.binMap = new HashMap<Integer, Integer>();
+        for(Integer key = low; key <= high; key++){
+            binMap.put(key, 0);
+        }
+        System.out.println(binMap);
+    }
+
+    public void incrementBin(Integer key){
+        binMap.put(key, binMap.get(key) + 1);
+    }
+
+    public Integer getBin(Integer key){
+        return binMap.get(key);
+    }
 }
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,17 @@
 public class Dice {
 
+    int numOfDice;
 
+    public Dice(int numOfDice) {
+        this.numOfDice = numOfDice;
+    }
+
+    public int tossAndSum() {
+        int sum = 0;
+        for (int i = 0; i < numOfDice; i++) {
+            sum += (int) (Math.random() * 6) + 1;
+            System.out.println("One dice roll " + sum);
+        }
+        return sum;
+    }
 }
